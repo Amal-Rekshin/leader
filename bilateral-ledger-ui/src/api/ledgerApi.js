ba2e370bf87061425551baee748d2ae84b975aec
Antony Amal Rekshin <antonyamalrekshingmail.com@Antonys-MacBook-Air.local>
2026-06-13 21:57:29 +0530

    Update URI

--- bilateral-ledger-ui/src/api/ledgerApi.js
+++ bilateral-ledger-ui/src/api/ledgerApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://leader-0ff5.onrender.com/api';
 const api = axios.create({ baseURL: API_BASE_URL });
 
 api.interceptors.request.use((config) => {
